cdb0d93eca0fe60cb087d9c9a6d74e9635184a15
ShoutaKatayama114514 <shota_katayama@unirita.co.jp>
2018-06-21 17:09:31 +0900

    作成途中。進数表記がよくわからず停止中

---
+++ problems3/Main.java
@@ -0,0 +1,39 @@
+package problems3;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		 n;
+		
+		int A = Integer.parseInt(sc.nextLine());
+		String[] a;
+		n = sc.nextLine();
+		a = n.split(" ");
+		
+		int B = Integer.parseInt(sc.nextLine());
+		String[] b;
+		n = sc.nextLine();
+		b = n.split(" ");
+		
+		int C = Integer.parseInt(sc.nextLine());
+		String[] c;
+		n = sc.nextLine();
+		c = n.split(" ");
+		
+		int K = Integer.parseInt(sc.nextLine());
+		String S = sc.nextLine();
+		String T = sc.nextLine();
+		for(int i = 0;i < S.length();i++) {
+			for(int j = 0;j < A;j++) {
+				if(true) {
+					
+				}
+			}
+		}
+	}
+
+}
